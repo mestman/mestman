b4419b9c0d1e6d203656d22fa3fe423d877b040a
yonghwankim-dev <dragonbead95@naver.com>
2024-08-17 12:09:12 +0900

    feat: Symbol 클래스에 level 추가

--- src/main/java/site/mestman/symbol/Symbol.java
+++ src/main/java/site/mestman/symbol/Symbol.java
@@ -1,23 +1,39 @@
 package site.mestman.symbol;
 
 import java.time.LocalDate;
+import java.util.stream.IntStream;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 public class Symbol {
+	private final int level;
 
 	// 현재 심볼이 최대 레벨을 달성하기 위해서 필요한 일자를 계산
 	public LocalDate calculateCompletionDateForMaxLevel(int numberOfSymbolPerDay) {
 		// 만렙 달성 필요 일자 = (만렙 필요 성장치 - 누적 성장치) / 하루에 얻을 수 있는 심볼 개수
 		// 누적 성장치 = 현재 레벨 누적 성장치 + 현재 레벨 성장치
 		int requiredMaxLevelGrowth = 2679;
-		int reduceGrowth = 0;
+		int growthByCurrentLevel = 0;
+		int reduceGrowth = reduceGrowthBy(level) + growthByCurrentLevel;
+		log.info("reduceGrowth is {} by level", reduceGrowth);
 		int days = (requiredMaxLevelGrowth - reduceGrowth) / numberOfSymbolPerDay;
 		if (requiredMaxLevelGrowth % numberOfSymbolPerDay != 0) {
 			days++;
 		}
 		log.info("days is {}", days);
 		return LocalDate.now().plusDays(days);
+	}
+
+	// 현재 레벨에 따른 누적 성장치 계산
+	private int reduceGrowthBy(int level) {
+		int[] requiredGrowthByLevel = {0, 12, 15, 20, 27, 36, 47, 60, 75, 92, 111, 132, 155, 180, 207, 236, 267, 300,
+			335, 372, 0};
+
+		return IntStream.rangeClosed(1, level - 1)
+			.map(i -> requiredGrowthByLevel[i])
+			.sum();
 	}
 }
--- src/test/java/site/mestman/symbol/SymbolTest.java
+++ src/test/java/site/mestman/symbol/SymbolTest.java
@@ -1,22 +1,34 @@
 package site.mestman.symbol;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SymbolTest {
 
 	@DisplayName("심볼이 주어지고 해당 심볼이 만렙을 달성하기 위한 필요일자를 계산한다")
-	@Test
-	void calculateDateForMaxLevel() {
+	@MethodSource(value = {"symbolSource"})
+	@ParameterizedTest
+	void calculateDateForMaxLevel(int level, LocalDate expected) {
 		// given
-		Symbol pathOfVanishing = new Symbol();
+		Symbol pathOfVanishing = new Symbol(level);
 		int numberOfSymbolPerDay = 20;
 		// when
-		LocalDate requiredDate = pathOfVanishing.calculateCompletionDateForMaxLevel(numberOfSymbolPerDay);
+		LocalDate completionDate = pathOfVanishing.calculateCompletionDateForMaxLevel(numberOfSymbolPerDay);
 		// then
-		Assertions.assertThat(requiredDate).isEqualTo(LocalDate.now().plusDays(134));
+		Assertions.assertThat(completionDate).isEqualTo(expected);
+	}
+
+	public static Stream<Arguments> symbolSource() {
+		LocalDate today = LocalDate.now();
+		return Stream.of(
+			Arguments.of(1, today.plusDays(134)),
+			Arguments.of(10, today.plusDays(115))
+		);
 	}
 }
